f48c00dc28bc1beee0d5c7eae77c1e1a72eb6f0f
Dennis C. <dpclements@proton.me>
2024-09-04 19:49:09 -0700

    removed dependency

--- bar-functions/dwm_backlight.sh
+++ bar-functions/dwm_backlight.sh
@@ -3,10 +3,11 @@
 # Dependencies: light
 
 PREFIX="󱍖"
+BAT_PERC=$(expr $(cat /sys/class/backlight/amdgpu_bl1/actual_brightness) \* 100 / $(cat /sys/class/backlight/amdgpu_bl1/max_brightness))
 
 dwm_backlight()
 {
-    echo ""$SEP1"$PREFIX  $(light | sed 's/\..*//g')%$SEP2"
+    echo ""$SEP1"$PREFIX  $BAT_PERC%$SEP2"
 }
 
 dwm_backlight
